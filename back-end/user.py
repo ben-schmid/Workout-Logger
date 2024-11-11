d5646b72d01fe1ab0490c48a00f4acbc5b7d59cb
Ben <benjamin.clark99@gmail.com>
2024-11-10 21:31:20 -0500

    Changed to mango Atlas

--- back-end/user.py
+++ back-end/user.py
@@ -8,18 +8,30 @@ class User:
         self.password = password
         self.name = name
         try:
-            self.client = MongoClient('localhost', 27017) #27017 is default port number for MongoDB
+            username = "ClarkBen1225"
+            atlas_password = "Atlas1225"
+            dbname = "mydb"
+            connection = f'mongodb+srv://{username}:{atlas_password}@cluster0.x20eh.mongodb.net/{dbname}?retryWrites=true&w=majority&appName=Cluster0'
+            self.client = MongoClient(connection) 
             self.db = self.client['mydb']
             self.users_collection = self.db['user_accounts']
+            print("Connection Made")
         except Exception as e:
             print(f"Error connecting to MongoDB: {e}")
 
     @staticmethod
     def validate_login(email, password):
         try:
-            client = MongoClient('localhost', 27017)
-            db = client['mydb']
+            username = "ClarkBen1225"
+            atlas_password = "Atlas1225"
+            dbname = "mydb"
+            connection = f'mongodb+srv://{username}:{atlas_password}@cluster0.x20eh.mongodb.net/{dbname}?retryWrites=true&w=majority&appName=Cluster0'
+
+            client = MongoClient(connection) 
+            db = client[dbname]
             users_collection = db['user_accounts']
+            print("Connection Made for login")
+            
             user = users_collection.find_one({"email": email})
             if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
                 return True

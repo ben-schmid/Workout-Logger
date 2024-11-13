b316e3aa5827bb7229c1dbb07c742ad31e44f3ce
ben-schmid <ben.schmid1@gmail.com>
2024-11-13 15:20:14 -0500

    updating .gitignore

--- front-end/src/pages/workouts/pplx4.js
+++ front-end/src/pages/workouts/pplx4.js
@@ -48,7 +48,7 @@ function createData(exercise, sets, reps, weight){
     return { exercise, sets, reps, weight};
 }
 
-const rows1 = [ 
+const day1 = [ 
     createData('Bench Press', 4, '6-8', ' '),
     createData('Overhead Shoulder Press', 4, '6-8',' '),
     createData('Dumbell Incline Press:', 3, 10,' '),
@@ -56,7 +56,7 @@ const rows1 = [
     createData('Tricep Dips', 3,'8-10',' '),
     createData('Tricep Rope Pushdown', 3, 12, ' '),
 ];
-const rows2 = [ 
+const day2 = [ 
     createData('Barbell Rows', 4, '8', ' '),
     createData('Lat Pulldowns', 4, '10',' '),
     createData('Face Pulls:', 3, 15,' '),
@@ -81,7 +81,7 @@ export default function Pplx4(){
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows1.map((row) =>(
+                            {day1.map((row) =>(
                                 <StyledTableRow key = {row.exercise}>
                                     <StyledTableCell component="th" scope="row">
                                         {row.exercise}
@@ -105,7 +105,7 @@ export default function Pplx4(){
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows2.map((row) =>(
+                            {day2.map((row) =>(
                                 <StyledTableRow key = {row.exercise}>
                                     <StyledTableCell component="th" scope="row">
                                         {row.exercise}

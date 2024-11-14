ad97365578d3b60dfa8c20c20eb9cb42b52dcbe6
ben-schmid <ben.schmid1@gmail.com>
2024-11-14 14:35:49 -0500

    fixed minor issues with table

--- front-end/src/pages/workouts/pplx4.js
+++ front-end/src/pages/workouts/pplx4.js
@@ -7,8 +7,14 @@ import WorkoutTable from '../../components/WorkoutTable';
 function createData(exercise, sets, reps, weight){
     return { exercise, sets, reps, weight};
 }
-
-const rows1 = [ 
+const day1 = [
+    createData('Squats', 4, '6-8', ''),
+    createData('Romanian Deadlifts', 4, 10,''),
+    createData('Walking Lunges', 3, '10 (per leg', ''),
+    createData('Hamstring Curls', 3, 15, ''),
+    createData('Calf Raises', 4, 15, ' ')
+];
+const day2 = [ 
     createData('Bench Press', 4, '6-8', ' '),
     createData('Overhead Shoulder Press', 4, '6-8',' '),
     createData('Dumbell Incline Press:', 3, 10,' '),
@@ -16,7 +22,7 @@ const rows1 = [
     createData('Tricep Dips', 3,'8-10',' '),
     createData('Tricep Rope Pushdown', 3, 12, ' '),
 ];
-const rows2 = [ 
+const day3 = [ 
     createData('Barbell Rows', 4, '8', ' '),
     createData('Lat Pulldowns', 4, '10',' '),
     createData('Face Pulls:', 3, 15,' '),
@@ -37,54 +43,14 @@ export default function Pplx4(){
         <>
             <Navbar/>
             <div className="content-below-navbar">
-                <StyledTableContainer component={Paper}>
-                    <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell>Exercise</StyledTableCell>
-                                <StyledTableCell>Sets</StyledTableCell>
-                                <StyledTableCell>Reps</StyledTableCell>
-                                <StyledTableCell>Weight</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows1.map((row) =>(
-                                <StyledTableRow key = {row.exercise}>
-                                    <StyledTableCell component="th" scope="row">
-                                        {row.exercise}
-                                    </StyledTableCell>
-                                    <StyledTableCell>{row.sets}</StyledTableCell>
-                                    <StyledTableCell>{row.reps}</StyledTableCell>
-                                    <StyledTableCell>{row.weight}</StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </StyledTableContainer>
-                <StyledTableContainer component={Paper}>
-                    <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell>Exercise</StyledTableCell>
-                                <StyledTableCell>Sets</StyledTableCell>
-                                <StyledTableCell>Reps</StyledTableCell>
-                                <StyledTableCell>Weight</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows2.map((row) =>(
-                                <StyledTableRow key = {row.exercise}>
-                                    <StyledTableCell component="th" scope="row">
-                                        {row.exercise}
-                                    </StyledTableCell>
-                                    <StyledTableCell>{row.sets}</StyledTableCell>
-                                    <StyledTableCell>{row.reps}</StyledTableCell>
-                                    <StyledTableCell>{row.weight}</StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </StyledTableContainer>
+                <h1> DAY 1: LEGS </h1>
+                <WorkoutTable data={day1}/>
+                <h1> DAY 2: PUSH </h1>
+                <WorkoutTable data={day2}/>
+                <h1> DAY 3: PULL </h1>
+                <WorkoutTable data={day3}/>
+                <h1> DAY 4: FULL BODY </h1>
+                <WorkoutTable data={day4}/>
             </div>
         
         </>
